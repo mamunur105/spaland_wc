3fd0cdd4b0702414e5c5584cd334b869f996df21
mamunur105 <rmamunur105@gmail.com>
2018-09-26 01:47:25 +0600

    admin user new field

--- spaland/cs-framework-override/config/shortcode.config.php
+++ spaland/cs-framework-override/config/shortcode.config.php
@@ -1,44 +1,47 @@
-<?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
-// ===============================================================================================
-// -----------------------------------------------------------------------------------------------
-// SHORTCODE GENERATOR OPTIONS
-// -----------------------------------------------------------------------------------------------
-// ===============================================================================================
-$options       = array();
+<?php
 
-// -----------------------------------------
-// Basic Shortcode Examples                -
-// -----------------------------------------
-$options[]     = array(
-  'title'      => 'Basic Shortcode Examples',
-  'shortcodes' => array(
+//  if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
+// // ===============================================================================================
+// // -----------------------------------------------------------------------------------------------
+// // SHORTCODE GENERATOR OPTIONS
+// // -----------------------------------------------------------------------------------------------
+// // ===============================================================================================
+// $options       = array();
 
-    // begin: shortcode
-    array(
-      'name'      => 'cs_shortcode_1',
-      'title'     => 'Basic Shortcode 1',
-      'fields'    => array(
-        // shortcode option field
-        array(
-          'id'    => 'title',
-          'type'  => 'text',
-          'title' => 'Title',
-        ),
+// // -----------------------------------------
+// // Basic Shortcode Examples                -
+// // -----------------------------------------
+// $options[]     = array(
+//   'title'      => 'Basic Shortcode Examples',
+//   'shortcodes' => array(
 
-        // shortcode content
-        array(
-          'id'    => 'content',
-          'type'  => 'textarea',
-          'title' => 'Content',
-          'help'  => 'Lorem Ipsum Dollar.',
-        )
+//     // begin: shortcode
+//     array(
+//       'name'      => 'cs_shortcode_1',
+//       'title'     => 'Basic Shortcode 1',
+//       'fields'    => array(
+//         // shortcode option field
+//         array(
+//           'id'    => 'title',
+//           'type'  => 'text',
+//           'title' => 'Title',
+//         ),
 
-      ),
-    ),
-    // end: shortcode
+//         // shortcode content
+//         array(
+//           'id'    => 'content',
+//           'type'  => 'textarea',
+//           'title' => 'Content',
+//           'help'  => 'Lorem Ipsum Dollar.',
+//         )
 
-  ),
-);
+//       ),
+//     ),
+
+//     // end: shortcode
+
+//   ),
+// );
 
 
-CSFramework_Shortcode_Manager::instance( $options );
+// CSFramework_Shortcode_Manager::instance( $options );
