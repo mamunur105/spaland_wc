4ca5dde2ecb565c032105c4c9449ad01b4a1a4d4
mamunur105 <rmamunur105@gmail.com>
2019-01-05 05:18:08 +0600

    all added

---
+++ plugin-base.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: Elementor Test Extension
+ * Description: Custom Elementor extension.
+ * Plugin URI:  https://elementor.com/
+ * Version:     1.0.0
+ * Author:      Elementor
+ * Author URI:  https://elementor.com/
+ * Text Domain: elementor-test-extension
+ */
+
+
+
+
+require_once( __DIR__ . '/adons-elementor.php' );
+// echo "string";
