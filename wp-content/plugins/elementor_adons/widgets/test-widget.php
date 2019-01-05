3fff4533685d7a474fd720b1a13427df197d25ca
mamunur105 <rmamunur105@gmail.com>
2019-01-05 06:56:52 +0600

    elementor file

---
+++ wp-content/plugins/elementor_adons/widgets/test-widget.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * Elementor oEmbed Widget.
+ *
+ * Elementor widget that inserts an embbedable content into the page, from any given URL.
+ *
+ * @since 1.0.0
+ */
+class Elementor_oEmbed_Widget extends \Elementor\Widget_Base {
+
+	/**
+	 * Get widget name.
+	 *
+	 * Retrieve oEmbed widget name.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'oembed';
+	}
+
+	/**
+	 * Get widget title.
+	 *
+	 * Retrieve oEmbed widget title.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Title', 'plugin-name' );
+	}
+
+	/**
+	 * Get widget icon.
+	 *
+	 * Retrieve oEmbed widget icon.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'eicon-type-tool';
+	}
+
+	/**
+	 * Get widget categories.
+	 *
+	 * Retrieve the list of categories the oEmbed widget belongs to.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return [ 'second-category' ];
+	}
+
+	/**
+	 * Register oEmbed widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function _register_controls() {
+
+
+		$this->start_controls_section(
+			'content_title',
+			[
+				'label' => __( 'Secound section', 'plugin-name' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+		$this->add_control(
+			'title',
+			[
+				'label' => __( 'Section Title', 'plugin-name' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+			]
+		);
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'style_section',
+			[
+				'label' => __( 'Style Section', 'plugin-name' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+		$this->add_responsive_control(
+			'title_padding',
+			[
+				'label' => __( 'Padding', 'plugin-name' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', 'em', '%' ],
+				'selectors' => [
+					'{{WRAPPER}} .main-title' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+		$this->add_responsive_control(
+
+			'font_size',
+			[
+				'label' => __( 'Size', 'plugin-name' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', 'em', 'rem' ],
+				'range' => [
+					'px' => [
+						'min' => 1,
+						'max' => 200,
+					],
+				],
+				'devices' => [ 'desktop', 'tablet', 'mobile' ],
+				'desktop_default' => [
+					'size' => 30,
+					'unit' => 'px',
+				],
+				'tablet_default' => [
+					'size' => 20,
+					'unit' => 'px',
+				],
+				'mobile_default' => [
+					'size' => 10,
+					'unit' => 'px',
+				],
+				'selectors' => [
+					'{{WRAPPER}} .main-title' => 'font-size: {{SIZE}}{{UNIT}}',
+				],
+				
+			]
+		);
+
+		$this->end_controls_section();
+
+	}
+
+	/**
+	 * Render oEmbed widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function render() {
+
+		$settings = $this->get_settings_for_display();
+		echo '<h2 class="main-title">'.$settings['title'].'</h2>';
+
+	}
+	protected function _content_template() { ?>
+		<h2>{{{ settings.title }}}</h2>
+
+	<?php	
+	 }
+
+
+}
